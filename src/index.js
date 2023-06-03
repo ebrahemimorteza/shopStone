31ee19181e26f5a87077733b73fa9427c9bd26f5
e._morteza11 <ebrahemimorteza10@gmail.com>
2023-06-03 22:42:03 +0330

    section 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const tick = ()=>{
+    const elem=(
+    <div>
+    <h1>time day</h1>
+    {new Date().toLocaleTimeString()}
+    </div>
+    )
+    ReactDOM.render( elem, document.getElementById('root'));  
+}
+setInterval(()=>{tick()},1000)
