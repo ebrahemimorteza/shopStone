0697d78517cde1f80e06c1a1ab71a2d7b2abfc1b
e._morteza11 <ebrahemimorteza10@gmail.com>
2023-06-04 11:07:40 +0330

    unit 11=>life cycle

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
-
+var interval;
+var timer=100;
+var timer2;
 class Timer extends React.Component{
     constructor(){
         super();
@@ -9,13 +11,30 @@ class Timer extends React.Component{
             time:new Date().toLocaleTimeString()
         }
     }
+    componentDidMount(){
+        console.log("componentDidMount");
+        interval = setInterval(()=>{
+            this.setState({time:new Date().toLocaleTimeString()})
+        },1000)
+        setInterval(()=>{
+            timer--;
+        },1000)
+    }
+    componentDidUpdate(){
+        console.log("componentDidUpdate");
+        if(timer===90){
+            console.log("you winnnnnnnnn");
+        }
+    }
+    componentWillUnmount(){
+        console.log("componentWillUnmount");
+    }
 render(){
-    setInterval(()=>{
-        this.setState({time:new Date().toLocaleTimeString()})
-    },1000)
+   console.log("render");
     return(
         <div>
         {this.state.time}
+        {timer}
         </div>
     )
 }
